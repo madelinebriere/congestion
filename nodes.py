572a4651c917b40800a28c1729446d62627d43d5
Maddie Briere <mfb33@duke.edu>
2018-09-19 23:05:04 -0400

    Basic Script

---
+++ nodes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python                                                                                                                                                         
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+class NodeGenerator():
+    "Connect two hosts."
+    def build(self):
+	host1 = self.addHost('transmitter')
+	host2 = self.addHost('receiver')
+	self.addLink(host1, host2)
+
+def launchNet():
+    "Create and test a 2-node network"
+    topo = NodeGenerator()
+    net = Mininet(topo)
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections(net.hosts)
+    print "Testing network connectivity"
+    net.pingAll()
+    net.stop()
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    launchNet()
