bfd92af77920816c095b79cb6a987f44a0e8ec69
Maddie Briere <mfb33@duke.edu>
2018-10-11 11:38:25 -0400

    New plots!

--- plot.py
+++ plot.py
@@ -5,7 +5,11 @@ import math
 from scipy.optimize import curve_fit
 
 def func(x, a, b):
-    return a+b*np.log(x);
+    return a+b*np.log(x)
+
+def func_lin(x, a, b):
+    exp = -1/2
+    return a + b*x**exp
 
 def plot():
     data = helper.read_list("output/data.txt")
@@ -14,12 +18,14 @@ def plot():
     popt, pcov = curve_fit(func, loss, y)
     y_plot = func(loss, *popt)
 
-    popt_calc, pcov_calc = curve_fit(func, loss, np.log(y))
-    print "C = %s" % -popt_calc[1]
+    popt_calc, pcov_calc = curve_fit(func_lin, loss, y)
+    print "C = %s" % popt_calc[1]
     
     format(helper.plt)
-    helper.plt.scatter(loss, y)
-    helper.plt.plot(loss, y_plot, linestyle='dashed')
+    d1 = helper.plt.scatter(loss, y, label='Reno')
+    d2 = helper.plt.plot(loss, y_plot, 
+        label='Model, C = %f' % popt_calc[1], linestyle='dashed')
+    helper.plt.legend()
     helper.plt.savefig("figure_reproduced.png")
 
 def format(plot):
