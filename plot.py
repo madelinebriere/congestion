7f9a697a21478b1ce2813ad9c8badfec919b8925
Maddie Briere <mfb33@duke.edu>
2018-10-11 11:13:46 -0400

    New data with trendline

--- plot.py
+++ plot.py
@@ -1,21 +1,29 @@
 import os
 import numpy as np
 import helper
+import math
+from scipy.optimize import curve_fit
+
+def func(x, a, b):
+    return a+b*np.log(x);
 
 def plot():
     data = helper.read_list("output/data.txt")
     loss = [float(item[0]) for item in data]
     y = [float(item[1]) for item in data]
-    loss_theory = [.0001, .05]
-    y_theory = [1.0/helper.math.sqrt(item) for item in loss_theory]
+    popt, pcov = curve_fit(func, loss, y)
+    y_plot = func(loss, *popt)
+
+    popt_calc, pcov_calc = curve_fit(func, loss, np.log(y))
+    print "C = %s" % -popt_calc[1]
     
     format(helper.plt)
     helper.plt.scatter(loss, y)
-    helper.plt.plot(loss_theory, y_theory, linestyle='dashed')
+    helper.plt.plot(loss, y_plot, linestyle='dashed')
     helper.plt.savefig("figure_reproduced.png")
 
 def format(plot):
-    plot.xlabel("loss")
+    plot.xlabel("loss (%)")
     plot.ylabel("BW*RTT/MSS")
     plot.xscale('log')
 
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo sysctl -w net.ipv4.tcp_congestion_control=reno
+sudo apt-get install python-numpy python-scipy python-matplotlib
 sudo mn -c # clear environment
 sudo python simulate.py
 sudo python plot.py
